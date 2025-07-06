797bb9bd8c47196b422887443030bc0a81285937
!Taka dev <kontol543215@gmail.com>
2025-07-06 18:31:48 +0700

    Create useAnimeDetail.ts

---
+++ hooks/useAnimeDetail.ts
@@ -0,0 +1,22 @@
+// hooks/useAnimeDetail.ts
+import useSWR from 'swr'
+import { fetchFromAnilist } from '@/lib/anilist'
+import { ANIME_DETAIL_QUERY } from '@/graphql/queries'
+import { AnimeDetail } from '@/types/anime'
+
+interface AnimeDetailResponse {
+  Media: AnimeDetail
+}
+
+export function useAnimeDetail(id: number) {
+  const { data, error, isLoading } = useSWR<AnimeDetailResponse>(
+    id ? ['ANIME_DETAIL', id] : null,
+    () => fetchFromAnilist(ANIME_DETAIL_QUERY, { id }),
+  )
+
+  return {
+    anime: data?.Media,
+    isLoading,
+    isError: error,
+  }
+}
