0d4643ba6e06601cd90356c650346be31a5e6b4e
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:08:23 +0700

    Update useTrendingAnime.ts

--- hooks/useTrendingAnime.ts
+++ hooks/useTrendingAnime.ts
@@ -1,24 +1,11 @@
 // hooks/useTrendingAnime.ts
-import useSWR from 'swr'
-import { fetchFromAnilist } from '@/lib/anilist'
-import { TRENDING_ANIME_QUERY } from '@/graphql/queries'
+import { useQuery } from '@tanstack/react-query'
+import { fetchTrendingAnime } from '@/lib/anilist'
 import { Anime } from '@/types/anime'
 
-interface TrendingAnimeResponse {
-  Page: {
-    media: Anime[]
-  }
-}
-
-export function useTrendingAnime(page = 1, perPage = 10) {
-  const { data, error, isLoading } = useSWR<TrendingAnimeResponse>(
-    ['TRENDING_ANIME', page, perPage],
-    () => fetchFromAnilist(TRENDING_ANIME_QUERY, { page, perPage }),
-  )
-
-  return {
-    anime: data?.Page.media ?? [],
-    isLoading,
-    isError: error,
-  }
+export function useTrendingAnime() {
+  return useQuery<Anime[]>({
+    queryKey: ['trendingAnime'],
+    queryFn: fetchTrendingAnime,
+  })
 }
