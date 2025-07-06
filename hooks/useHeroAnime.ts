2a1e73334f711aaa5561a14372a64b799fda94fe
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:08:00 +0700

    Update useHeroAnime.ts

--- hooks/useHeroAnime.ts
+++ hooks/useHeroAnime.ts
@@ -1,37 +1,14 @@
 // hooks/useHeroAnime.ts
 import { useQuery } from '@tanstack/react-query'
+import { fetchTrendingAnime } from '@/lib/anilist'
 import { Anime } from '@/types/anime'
-import { fetchFromAnilist } from '@/lib/anilist'
 
 export function useHeroAnime() {
-  return useQuery<Anime>({
-    queryKey: ['hero-anime'],
+  return useQuery<Anime[]>({
+    queryKey: ['heroAnime'],
     queryFn: async () => {
-      const query = `
-        query {
-          Page(perPage: 1) {
-            media(type: ANIME, sort: POPULARITY_DESC, isAdult: false) {
-              id
-              title {
-                romaji
-                english
-              }
-              coverImage {
-                large
-              }
-              bannerImage
-              genres
-              averageScore
-              trailer {
-                id
-                site
-              }
-            }
-          }
-        }
-      `
-      const data = await fetchFromAnilist(query)
-      return data.Page.media[0]
+      const data = await fetchTrendingAnime()
+      return data.slice(0, 1) // hanya ambil satu untuk Hero
     },
   })
 }
