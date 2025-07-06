525cdbbbf7347d8c5363c02179598a0166c97a81
!Taka dev <kontol543215@gmail.com>
2025-07-06 18:24:29 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+      colors: {
+        primary: '#38bdf8', // warna biru elektrik
+        dark: '#0f0f0f',
+      },
+    },
+  },
+  plugins: [],
+}
