8a32607960c23e403578962ad8f631982deacbe9
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:08:43 +0700

    Update useOngoingAnime.ts

--- hooks/useOngoingAnime.ts
+++ hooks/useOngoingAnime.ts
@@ -1,38 +1,11 @@
 // hooks/useOngoingAnime.ts
 import { useQuery } from '@tanstack/react-query'
+import { fetchOngoingAnime } from '@/lib/anilist'
 import { Anime } from '@/types/anime'
-import { fetchFromAnilist } from '@/lib/anilist'
 
 export function useOngoingAnime() {
   return useQuery<Anime[]>({
     queryKey: ['ongoingAnime'],
-    queryFn: async () => {
-      const query = `
-        query {
-          Page(perPage: 12) {
-            media(type: ANIME, status: RELEASING, sort: POPULARITY_DESC) {
-              id
-              title {
-                romaji
-                english
-              }
-              coverImage {
-                large
-              }
-              bannerImage
-              genres
-              averageScore
-              description
-              trailer {
-                id
-                site
-              }
-            }
-          }
-        }
-      `
-      const data = await fetchFromAnilist(query)
-      return data?.Page?.media ?? []
-    },
+    queryFn: fetchOngoingAnime,
   })
 }
