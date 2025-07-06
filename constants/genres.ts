b74c1f4208d0f3665d800d78a778476430e15cac
!Taka dev <kontol543215@gmail.com>
2025-07-06 18:46:08 +0700

    Create genres.ts

---
+++ constants/genres.ts
@@ -0,0 +1,19 @@
+// constants/genres.ts
+export const GENRES = [
+  'Action',
+  'Adventure',
+  'Comedy',
+  'Drama',
+  'Fantasy',
+  'Horror',
+  'Mecha',
+  'Music',
+  'Mystery',
+  'Psychological',
+  'Romance',
+  'Sci-Fi',
+  'Slice of Life',
+  'Sports',
+  'Supernatural',
+  'Thriller'
+]
