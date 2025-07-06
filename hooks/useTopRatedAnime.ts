77cef5f255ffa003ffe8b52a65451b7ed3885c45
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:09:23 +0700

    Update useTopRatedAnime.ts

--- hooks/useTopRatedAnime.ts
+++ hooks/useTopRatedAnime.ts
@@ -1,37 +1,11 @@
 // hooks/useTopRatedAnime.ts
 import { useQuery } from '@tanstack/react-query'
+import { fetchTopRatedAnime } from '@/lib/anilist'
 import { Anime } from '@/types/anime'
-import { fetchFromAnilist } from '@/lib/anilist'
 
 export function useTopRatedAnime() {
   return useQuery<Anime[]>({
     queryKey: ['topRatedAnime'],
-    queryFn: async () => {
-      const query = `
-        query {
-          Page(perPage: 12) {
-            media(type: ANIME, sort: SCORE_DESC) {
-              id
-              title {
-                romaji
-                english
-              }
-              coverImage {
-                large
-              }
-              bannerImage
-              genres
-              averageScore
-              trailer {
-                id
-                site
-              }
-            }
-          }
-        }
-      `
-      const data = await fetchFromAnilist(query)
-      return data?.Page?.media ?? []
-    }
+    queryFn: fetchTopRatedAnime,
   })
 }
