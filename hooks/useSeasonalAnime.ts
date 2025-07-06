664032abe2859d9c520550d35e1d9f83ce642563
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:09:03 +0700

    Update useSeasonalAnime.ts

--- hooks/useSeasonalAnime.ts
+++ hooks/useSeasonalAnime.ts
@@ -1,38 +1,11 @@
 // hooks/useSeasonalAnime.ts
 import { useQuery } from '@tanstack/react-query'
+import { fetchSeasonalAnime } from '@/lib/anilist'
 import { Anime } from '@/types/anime'
-import { fetchFromAnilist } from '@/lib/anilist'
 
 export function useSeasonalAnime() {
   return useQuery<Anime[]>({
     queryKey: ['seasonalAnime'],
-    queryFn: async () => {
-      const query = `
-        query {
-          Page(perPage: 12) {
-            media(season: SUMMER, seasonYear: 2024, type: ANIME, sort: POPULARITY_DESC) {
-              id
-              title {
-                romaji
-                english
-              }
-              coverImage {
-                large
-              }
-              bannerImage
-              genres
-              averageScore
-              description
-              trailer {
-                id
-                site
-              }
-            }
-          }
-        }
-      `
-      const data = await fetchFromAnilist(query)
-      return data?.Page?.media ?? []
-    }
+    queryFn: fetchSeasonalAnime,
   })
 }
