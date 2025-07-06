43fd5db50db33239527368d1f5adcc5173fdad8b
!Taka dev <kontol543215@gmail.com>
2025-07-06 22:43:43 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import Navbar from '@/components/common/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Navbar />
       <Component {...pageProps} />
     </QueryClientProvider>
   )
