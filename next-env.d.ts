1caedc510d281fe69a5239b76bc237d249fd8249
!Taka dev <kontol543215@gmail.com>
2025-07-06 19:02:38 +0700

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+// next-env.d.ts
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
