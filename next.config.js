8ed29964012b8ae263d11079539181d81d2ad795
!Taka dev <kontol543215@gmail.com>
2025-07-07 10:55:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['s4.anilist.co', 'uploads.mangadex.org'],
+    domains: [
+      's4.anilist.co', // anime image
+      'uploads.mangadex.org', // ✅ manga cover
+      'mangadex.org', // optional
+    ],
   },
 }
 
