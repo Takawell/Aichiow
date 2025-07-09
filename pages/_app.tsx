dcdbb113b1558a9dd0487c2463032df573c11306
!Taka dev <kontol543215@gmail.com>
2025-07-10 05:32:47 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 import Navbar from '@/components/layout/Navbar'
+import UpdateModal from '@/components/shared/UpdateModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
