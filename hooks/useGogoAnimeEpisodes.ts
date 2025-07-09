7de06a86db43ef21e11a1b5a5e14456b1a08caad
!Taka dev <kontol543215@gmail.com>
2025-07-09 07:23:19 +0700

    Create useGogoAnimeEpisodes.ts

---
+++ hooks/useGogoAnimeEpisodes.ts
@@ -0,0 +1,19 @@
+// hooks/useGogoAnimeEpisodes.ts
+
+import useSWR from "swr"
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+export function useGogoAnimeEpisodes(slug: string | undefined) {
+  const shouldFetch = !!slug
+  const { data, error, isLoading } = useSWR(
+    shouldFetch ? `https://api.consumet.org/anime/gogoanime/info/${slug}` : null,
+    fetcher
+  )
+
+  return {
+    episodes: data?.episodes || [],
+    isLoading,
+    isError: !!error
+  }
+}
