fe12f93f8d1c3bb480c01c57c171fc9fe5da3b53
!Taka dev <kontol543215@gmail.com>
2025-07-10 17:09:18 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,21 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    'aspect-[21/7]',
+    'keen-slider',
+    'keen-slider__slide',
+  ],
   theme: {
     extend: {
       colors: {
         dark: '#111111',
-        primary: '#38bdf8', // Warna utama Aichiow
+        primary: '#38bdf8',
+      },
+      aspectRatio: {
+        '21/7': [21, 7],
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 }
