ccb155d8877cbf6be6d6f4224805ab33583879f9
!Taka dev <kontol543215@gmail.com>
2025-07-11 08:26:52 +0700

    Update MangaGrid.tsx

--- components/manga/MangaGrid.tsx
+++ components/manga/MangaGrid.tsx
@@ -1,3 +1,4 @@
+import { getLocalizedTitle, getCoverImage } from '@/lib/mangadex'
 import MangaCard from './MangaCard'
 
 interface MangaGridProps {
@@ -12,7 +13,7 @@ export default function MangaGrid({ mangaList }: MangaGridProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
       {mangaList.map((manga) => {
-        const title = manga.attributes.title?.en || manga.attributes.title?.ja || 'Untitled'
+        const title = getLocalizedTitle(manga.attributes.title)
         const cover = manga.relationships.find((rel: any) => rel.type === 'cover_art')
         const coverFileName = cover?.attributes?.fileName || ''
 
