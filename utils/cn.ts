7700f21d84d7d3385d1ce2f6f1ffe36d43fa82cc
!Taka dev <kontol543215@gmail.com>
2025-07-13 17:22:47 +0700

    Update cn.ts

--- utils/cn.ts
+++ utils/cn.ts
@@ -1,7 +1,3 @@
-import { ClassValue } from 'clsx'
-import clsx from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(...inputs))
+export function cn(...classes: (string | undefined | false | null)[]) {
+  return classes.filter(Boolean).join(' ')
 }
