fbe0b48d5f6eb0f928d7cf8e83c806f82bd6f3e9
!Taka dev <kontol543215@gmail.com>
2025-07-13 17:11:34 +0700

    Update sheet.tsx

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as Dialog from '@radix-ui/react-dialog'
-import { cn } from '@/utils/cn'
+import { cn } from '@/utils/classnames'
 
 export const Sheet = Dialog.Root
 export const SheetTrigger = Dialog.Trigger
