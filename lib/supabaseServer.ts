ccba899252747ca1742cfbf0019e18805f2b7754
!Taka dev <kontol543215@gmail.com>
2025-07-15 06:43:05 +0700

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,12 +1,22 @@
-import { cookies } from 'next/headers';
+import { cookies as nextCookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 
 export const createSupabaseServerClient = () => {
+  const cookieStore = nextCookies();
+
+  
+  const cookieMethods = {
+    get: (name: string) => {
+      const cookie = cookieStore.get(name);
+      return cookie?.value ?? null;
+    },
+    set: () => {}, // optional implement
+    delete: () => {} // optional implement
+  };
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: cookies()
-    }
+    { cookies: cookieMethods }
   );
 };
