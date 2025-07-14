883f0c7d6b39c792eb8fe7214987549d7a421c4f
!Taka dev <kontol543215@gmail.com>
2025-07-15 01:12:09 +0700

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,7 +1,10 @@
-// lib/supabaseServer.ts
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
 
 export const createSupabaseServerClient = () => {
-  return createServerComponentClient({ cookies });
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies }
+  );
 };
