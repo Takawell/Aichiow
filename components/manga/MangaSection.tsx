80e2967c25e0fbda3d1d421a1503fde04148e75e
!Taka dev <kontol543215@gmail.com>
2025-07-14 08:19:46 +0700

    Update MangaSection.tsx

--- components/manga/MangaSection.tsx
+++ components/manga/MangaSection.tsx
@@ -18,7 +18,12 @@ export const MangaSection = ({ title, mangas, icon }: Props) => {
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {mangas.map((manga) => (
-          <MangaCard key={manga.id} manga={manga} />
+          <MangaCard
+            key={manga.id}
+            id={manga.id}
+            title={manga.title}
+            coverFileName={manga.coverFileName}
+          />
         ))}
       </div>
     </section>
