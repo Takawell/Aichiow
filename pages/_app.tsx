171b47fa84505792eacc70d56fe48235b60e08f5
!Taka dev <kontol543215@gmail.com>
2025-07-16 11:13:27 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,17 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 import Navbar from '@/components/layout/Navbar'
-import UpdateModal from '@/components/shared/UpdateModal'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   const router = useRouter()
-  
+
   const isLanding = router.pathname === '/'
 
   return (
     <QueryClientProvider client={queryClient}>
       {!isLanding && <Navbar />}
-      {!isLanding && <UpdateModal />}
-      
       <Component {...pageProps} />
     </QueryClientProvider>
   )
