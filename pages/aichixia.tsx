9c89c0ed8b9afc6aa9e7020dd2c90a02690c8957
!Taka <kontol543215@gmail.com>
2025-07-23 13:58:05 +0700

    Delete pages/aichixia.tsx

--- pages/aichixia.tsx
+++
@@ -1,9 +0,0 @@
-import ChatBox from "@/components/Aichixia/ChatBox";
-
-export default function AichixiaPage() {
-  return (
-    <div className="aichixia-page">
-      <ChatBox />
-    </div>
-  );
-}
