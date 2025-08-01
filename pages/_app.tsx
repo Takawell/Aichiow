c6e1a8f40c2018e71d295a3d5ff4ac16dd94a8a0
!Taka <kontol543215@gmail.com>
2025-08-01 19:48:23 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 import Navbar from '@/components/layout/Navbar'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
@@ -13,6 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {!isLanding && <Navbar />}
       <Component {...pageProps} />
     </QueryClientProvider>
