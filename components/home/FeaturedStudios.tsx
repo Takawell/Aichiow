054d3bfa436b4630fc84175678c8909d80485de9
!Taka <kontol543215@gmail.com>
2025-08-03 14:33:55 +0700

    Update FeaturedStudios.tsx

--- components/home/FeaturedStudios.tsx
+++ components/home/FeaturedStudios.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { fetchTopRatedAnime, fetchAnimeDetail } from '@/lib/anime'
+import { fetchTopRatedAnime, fetchAnimeDetail } from '@/lib/anilist'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 
