e9f67f5d11da3610087791cd691debdb71fffdec
!Taka <kontol543215@gmail.com>
2025-08-07 01:23:07 +0700

    Update AnimeCard.tsx

--- components/anime/AnimeCard.tsx
+++ components/anime/AnimeCard.tsx
@@ -28,7 +28,9 @@ export default function AnimeCard({ anime }: Props) {
 
         {/* TEXT */}
         <div className="relative z-20 p-3 h-[72px] flex flex-col justify-between">
-          <h3 className="text-sm font-semibold text-white group-hover:text-indigo-400 transition-colors duration-300 truncate leading-snug">
+          <h3
+            className="text-sm font-semibold text-white group-hover:text-indigo-400 transition-colors duration-300 truncate"
+          >
             {anime.title.english || anime.title.romaji}
           </h3>
 
