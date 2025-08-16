6f75d88ffc1ce85ec03761f7c902357d5ab544aa
!Taka <kontol543215@gmail.com>
2025-08-16 18:33:13 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Navbar from '@/components/layout/Navbar'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import { supabase } from '@/lib/supabaseClient'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   const router = useRouter()
+
   const isLanding = router.pathname === '/'
-
-  useEffect(() => {
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === 'SIGNED_IN') {
-        router.replace('/profile') // redirect setelah login
-      } else if (event === 'SIGNED_OUT') {
-        router.replace('/auth/login') // redirect setelah logout
-      }
-    })
-
-    return () => subscription.unsubscribe()
-  }, [router])
 
   return (
     <QueryClientProvider client={queryClient}>
