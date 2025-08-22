7308f47514823842bbf8d094ecc427d25ea17224
!Taka <kontol543215@gmail.com>
2025-08-22 14:46:41 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       {!isLanding && <Navbar />}
-
       <main className="md:pb-0 pb-[calc(var(--bottom-nav,72px)+env(safe-area-inset-bottom))]">
         <Component {...pageProps} />
       </main>
