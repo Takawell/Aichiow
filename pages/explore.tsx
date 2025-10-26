9be38b0aa6673d00fb0d9be8733edc4adfc42b88
!Taka <kontol543215@gmail.com>
2025-10-26 21:52:10 +0700

    Update explore

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -347,7 +347,7 @@ export default function ExplorePage() {
 
                 <div className="text-center mb-6 mt-2">
                   <h2 className="text-2xl font-extrabold bg-gradient-to-r from-blue-400 to-cyan-300 bg-clip-text text-transparent">
-                    🔎 Search Engine
+                    🔎 ANISCAN
                   </h2>
                   <p className="text-sm text-neutral-400 mt-1">Upload a screenshot to detect the anime instantly</p>
                 </div>
