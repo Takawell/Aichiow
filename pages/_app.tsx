2dfc3039e630e3181e2dba75c94ce185309e677f
!Taka <kontol543215@gmail.com>
2025-12-12 09:07:45 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import Navbar from '@/components/layout/Navbar'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import UpdateModal from '@/components/shared/UpdateModal'
@@ -26,6 +27,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {!isLanding && !isMaintenance && <Navbar />}
 
       {isLanding && <UpdateModal />}
 
